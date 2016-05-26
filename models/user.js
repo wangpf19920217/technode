f67a46d47293a4212c7ce707a54f6bde4fd8d897
Wpf <851118434@qq.com>
2016-05-26 16:51:16 +0800

    添加mongoDB 写接受user信息的models

---
+++ models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var User = new Schema({
+    email:string,
+    name:string,
+    avatarUrl:string
+});
+module.exports = User;
+// var User = new Schema({
+//   email: String,
+//   name: String,
+//   avatarUrl: String
+// });avatarUrl这个字段是根据用户给的邮箱地址计算出来的avatar头像地址；
+
+module.exports = User;
